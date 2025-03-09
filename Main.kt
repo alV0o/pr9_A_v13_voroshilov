c71c964193b8f9016fba1391d191fe63cd04a282
alV0o <voroshilov.olexandro07@mail.ru>
2025-03-10 00:53:17 +0500

    Add files via upload

---
+++ Main.kt
@@ -0,0 +1,63 @@
+fun main(){
+    try{
+        println("Введите название, Введите цену, Введите единицу измерения, Введите количество товаров")
+        val product1 = GroceryProduct(readLine()!!.toString(), readLine()!!.toInt(), readLine()!!.toString(), readLine()!!.toInt())
+
+        when{
+            (product1.price < 0) -> println("Неверная цена")
+            (product1.count < 0) -> println("Неверное количество товаров")
+            else ->{
+                println("Введите дату изготовления (одного месяца)")
+                product1.dayOfManufacture = readLine()!!.toInt()
+                println("Введите сегодняшнюю дату (одного месяца)")
+                product1.today = readLine()!!.toInt()
+
+                when{
+                    (product1.dayOfManufacture < 1 || product1.today < 1 || product1.today > 31 || product1.dayOfManufacture > 31) -> println("Неверные даты")
+                    (product1.today - product1.dayOfManufacture < 0) -> println("Сегодняшняя дата не может быть меньше предыдущей")
+                    else -> {
+                        product1.GetInfo()
+                        product1.PriceOfOneProduct()
+                        product1.CountBoxes()
+                        product1.Delivery()
+                    }
+                }
+            }
+        }
+
+        println("---------------")
+
+        println("Введите название, Введите цену, Введите единицу измерения, Введите количество товаров")
+        val product2 = HouseholdProduct(readLine()!!.toString(), readLine()!!.toInt(), readLine()!!.toString(), readLine()!!.toInt())
+
+        when{
+            (product2.price < 0) -> println("Неверная цена")
+            (product2.count < 0) -> println("Неверное количество товаров")
+            else -> {
+                product2.TypeOfHHProduct()
+                product2.PriceOfOneProduct()
+                product2.CountBoxes()
+                product2.Delivery()
+            }
+        }
+
+        println("----------------")
+
+        println("Введите название, Введите цену, Введите единицу измерения, Введите количество товаров")
+        val product3 = Tovar(readLine()!!.toString(), readLine()!!.toInt(), readLine()!!.toString(), readLine()!!.toInt())
+
+        when{
+            (product3.price < 0) -> println("Неверная цена")
+            (product3.count < 0) -> println("Неверное количество товаров")
+            else ->{
+                product3.GetInfo()
+                product3.PriceOfOneProduct()
+                product3.CountBoxes()
+                product3.Delivery()
+            }
+        }
+    }
+    catch (e:Exception){
+        println("Неверный формат")
+    }
+}
---
+++ Tovar.kt
@@ -0,0 +1,92 @@
+open class Tovar(var name:String, var price:Int, var unitsOfMeasuring:String, var count:Int) {
+    open var typeOfTovar = ""
+
+    open fun SetType(){
+        println("Выберите тип товара")
+        println("[1] - Бытовой")
+        println("[2] - Продуктовый")
+        println("[3] - Строительный")
+        println("[4] - Для гигиены")
+
+        val answer = readLine()!!.toInt()
+
+        when(answer){
+            1 -> typeOfTovar = "Бытовой"
+            2 -> typeOfTovar = "Продуктовый"
+            3 -> typeOfTovar = "Строительный"
+            4 -> typeOfTovar = "Для гигиены"
+            else ->{
+                println("Неверный тип товара")
+                SetType()
+            }
+        }
+    }
+
+    open fun GetInfo(){
+        SetType()
+        println("Вид товара: ${typeOfTovar}")
+        println("Название товара: ${name}")
+        println("Единицы измерения: ${unitsOfMeasuring}")
+        println("Количество: ${count}")
+        println("Цена: ${price}")
+    }
+
+    open fun PriceOfOneProduct(){
+        println("Цена за 1 товар: ${String.format("%.2f", price!!.toDouble()/count!!.toDouble())}")
+    }
+
+    fun CountBoxes(){
+        println("Введите объем упаковки 1 товара")
+        val volumeTovar = readLine()!!.toInt()
+        when{
+            (volumeTovar<=0) -> {
+                println("Неверный объем. Введите заново")
+                CountBoxes()
+            }
+        }
+        val volumeBox = 500
+
+        when{
+            (volumeTovar>volumeBox) -> println("Потребуется ${count} индивидуальных коробок")
+            else->{
+                val countInOneBox = Math.floor(volumeBox/volumeTovar!!.toDouble())//сколько
+                println("Потребуется ${Math.ceil(count/countInOneBox)} стандартных коробок")
+            }
+        }
+    }
+
+    fun Delivery(){
+        var extraPrice = 0.0 //наценка
+
+        println("Товар является хрупким?")
+        println("[1] - Да")
+        println("[2] - Нет")
+        val answer = readLine()!!.toInt()
+
+        when{
+            (answer == 1) -> extraPrice+=0.5
+            (answer != 2) ->{
+                println("Неверный вариант. Выберите заново")
+                Delivery()
+            }
+        }
+        println("----------")
+
+        println("Введите расстояние между пунктами назначения")
+        val range = readLine()!!.toInt()
+
+        when {
+            (range >= 10 && range < 50) -> extraPrice += 0.2
+            (range >= 50 && range < 100) -> extraPrice += 0.3
+            (range > 100) -> extraPrice += 0.5
+            else -> {
+                println("Неверное расстояние. Введите заново")
+                Delivery()
+            }
+        }
+
+        val defPrice = 1000
+
+        println("Доставка будет стоить: ${defPrice + defPrice*extraPrice}p")
+    }
+}
